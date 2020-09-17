7486519649b5eddc55c0aedc8cdc8306936e3abb
Marlon Fernandez <mfernandez@coadvantage.com>
2020-09-17 11:00:04 -0400

    Adding some others rules

--- src/components/VeeValidateForm.vue
+++ src/components/VeeValidateForm.vue
@@ -14,16 +14,16 @@
     <div class="row">
       <div class="col-md-6">
         <form>
+          <div class="form-group">
+            <label for="inputEmail4">Email</label>
+            <ValidationProvider name="Email" rules="required|email" v-slot="v">
+              <input type="email" class="form-control" id="inputEmail4" placeholder="Email"
+                     :class="v.classes"
+                     v-model="email">
+              <div class="invalid-feedback">{{ v.errors[0] }}</div>
+            </ValidationProvider>
+          </div>
           <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="inputEmail4">Email</label>
-              <ValidationProvider name="Email" rules="required|email" v-slot="v">
-                <input type="email" class="form-control" id="inputEmail4" placeholder="Email"
-                       :class="v.classes"
-                       v-model="email">
-                <div class="invalid-feedback">{{ v.errors[0] }}</div>
-              </ValidationProvider>
-            </div>
             <div class="form-group col-md-6">
               <label for="inputPassword4">Password</label>
               <ValidationProvider rules="secret" v-slot="v">
@@ -34,30 +34,58 @@
                 <div class="invalid-feedback">{{ v.errors[0] }}</div>
               </ValidationProvider>
             </div>
+            <div class="form-group col-md-6">
+              <label for="inputConfirmPassword4">Password</label>
+              <ValidationProvider rules="secret" v-slot="v">
+                <input type="password" class="form-control" id="inputConfirmPassword4" placeholder="Confirm Password"
+                       :class="v.classes"
+                       v-model="passwordConfirm"
+                >
+                <div class="invalid-feedback">{{ v.errors[0] }}</div>
+              </ValidationProvider>
+            </div>
           </div>
           <div class="form-group">
             <label for="inputAddress">Address</label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+            <ValidationProvider name="Address" rules="required" v-slot="v">
+              <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St"
+                     :class="v.classes"
+                     v-model="address"
+              >
+            </ValidationProvider>
           </div>
           <div class="form-group">
             <label for="inputAddress2">Address 2</label>
             <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
           </div>
+          <div class="form-group">
+            <label for="inputCity">City</label>
+            <ValidationProvider name="City" rules="required|minLength:3" v-slot="v">
+              <input type="text" class="form-control" id="inputCity"
+                     :class="v.classes"
+                     v-model="city"
+              >
+              <p class="invalid-feedback">{{ v.errors[0] }}</p>
+            </ValidationProvider>
+          </div>
           <div class="form-row">
-            <div class="form-group col-md-6">
-              <label for="inputCity">City</label>
-              <input type="text" class="form-control" id="inputCity">
-            </div>
-            <div class="form-group col-md-4">
+            <div class="form-group col-md-8">
               <label for="inputState">State</label>
               <select id="inputState" class="form-control">
                 <option selected>Choose...</option>
                 <option>...</option>
               </select>
             </div>
-            <div class="form-group col-md-2">
+            <div class="form-group col-md-4">
               <label for="inputZip">Zip</label>
-              <input type="text" class="form-control" id="inputZip">
+              <ValidationProvider name="Zip" rules="required|minMaxLength:5,10" v-slot="v">
+                <input type="text" class="form-control" id="inputZip" placeholder="34243-4419"
+                       pattern="^\d{5}(?:[-\s]\d{4})?$"
+                       :class="v.classes"
+                       v-model="zip"
+                >
+                <p class="invalid-feedback">{{v.errors[0]}}</p>
+              </ValidationProvider>
             </div>
           </div>
           <div class="form-group">
@@ -81,7 +109,13 @@ export default {
   data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      passwordConfirm: '',
+      address: '',
+      address2: '',
+      city: '',
+      state: '',
+      zip: ''
     }
   }
 }
--- src/validators.js
+++ src/validators.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import {ValidationProvider, extend, configure} from 'vee-validate';
-import {required,email} from "vee-validate/dist/rules";
+import {required,email,regex} from "vee-validate/dist/rules";
 
 
 // config
@@ -17,7 +17,38 @@ Vue.component('ValidationProvider', ValidationProvider);
 extend('required', required);
 extend('email', email);
 
+// registering existing rule with custom message
+extend('regex', {
+    ...regex,
+    message: '{_field_} most have the form 34243 or 34243-4419'
+});
+
 extend('secret', {
     validate: value => value === 'example',
     message: 'This is not the magic word'
+});
+
+// using parameter and placeholder on message
+extend('minLength', {
+    validate: (value, args) => {
+        return value.length >= args.length;
+    },
+    message: 'The {_field_} must have at least {length} characters',
+    params: ['length']
+});
+
+// using multiples parameters, destructuring args and message as function
+extend('minMaxLength', {
+    validate: (value, {min, max}) => {
+        return value.length >= min && value.length <= max;
+    },
+    message: (fieldName, {min, max} /*placeholders*/) => {
+        // you can use placeholders object
+        //return `${fieldName} must have at least ${placeholders.min} characters and ${placeholders.max} characters at most`;
+
+        // or destructure the placeholder
+        // more about message on https://logaretm.github.io/vee-validate/guide/basics.html#messages
+        return `${fieldName} must have at least ${min} characters and ${max} characters at most`;
+    },
+    params: ['min', 'max']
 });
